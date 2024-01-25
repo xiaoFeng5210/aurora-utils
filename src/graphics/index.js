5c8d4b05ec57bbca1b3434657d66155c43aa496c
Eadweard <804875728@qq.com>
2024-01-25 11:17:06 +0800

    向量运算

---
+++ src/graphics/index.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @param x 向量x
+ * @param y 向量y
+ * @returns 
+ */
+export function vectorLength(x, y) {
+  return Math.sqrt(x * x + y * y);
+}
+
+/**
+ * 向量加法
+ */
+export function vectorAdd(v1, v2) {
+  return [v1[0] + v2[0], v1[1] + v2[1]];
+}
+
+/**
+ * 向量数乘
+ */
+export function vectorMulti(v, num) {
+  return [v[0] * num, v[1] * num];
+}
+
+/**
+ * 向量点积
+ */
+export function vectorDot(v1, v2) {
+  return v1[0] * v2[0] + v1[1] * v2[1];
+}
