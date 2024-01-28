be9329c41c0b5345b3df90d94090d1a70e7c9df9
Eadweard <804875728@qq.com>
2024-01-28 16:14:39 +0800

    webgl工具包

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { add } from "./src/common";
 import { last } from "./src/array";
 import {calAngle } from "./src/graphics";
+import * as webglUtils from "./src/webgl/utils";
 
-export { add, last };
+export { add, last, webglUtils, calAngle};
