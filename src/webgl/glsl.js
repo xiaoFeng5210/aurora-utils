5178dd015083d5778912b31e3b8ecba5ed17c8d9
Eadweard <804875728@qq.com>
2024-01-28 18:36:12 +0800

    增加着色器代码

---
+++ src/webgl/glsl.js
@@ -0,0 +1,11 @@
+// 画圆的glsl代码
+export function circleSharder(params) {
+   return `
+     float dist = distance(gl_PointCoord, vec2(0.5, 0.5));
+     if (dist > 0.5) {
+       gl_FragColor = ${params}
+     } else {
+      discard;
+     }
+   `
+}
