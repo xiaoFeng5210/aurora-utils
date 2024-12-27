f91fa3114861a83066968fb62eb70ec834da5ecb
Eadweard <aurora.coder.zqf@gmail.com>
2024-12-27 14:13:22 +0800

    feat: arrAt函数

--- packages/array/index.ts
+++ packages/array/index.ts
@@ -30,3 +30,28 @@ export function last<T>(arr: T[], n?: number): T[] {
   }
   return []
 }
+
+
+/**
+ * 完成后端语法选取数组。-1表示最后一位，-2表示倒数第二位，以此类推。参数是一个负数，则表示从后往前数。参数是正整数，则表示从前往后数。
+ */
+export function arrAt<T>(arr: T[], index: number): T {
+  if (!Array.isArray(arr)) {
+    throw new Error('第一个参数必须是数组')
+  }
+  // 判断index是整数，不是浮点数
+  if (!Number.isInteger(index)) {
+    throw new Error('第二个参数必须是正整数或者负整数')
+  }
+  if (index < 0 && index >= -arr.length) {
+    return arr[arr.length + index]
+  }
+  if (index >= arr.length) {
+    return arr[arr.length - 1]
+  }
+  if (index < -arr.length) {
+    return arr[0]
+  }
+  return arr[index]
+}
+
